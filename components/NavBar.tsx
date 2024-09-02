d19db76e6e148a4d817ad5c773b9107e58bfb8f8
Wycliff Kimutai <kcliff@tutanota.com>
2024-09-02 17:11:16 +0300

    links dropdown from shadn for small screens

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,6 +1,18 @@
+import LinksDropdown from './LinksDropDown';
+import { UserButton } from '@clerk/nextjs';
+import ThemeToggle from './ThemeToggler';
+
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav className='bg-muted py-4 sm:px-16 lg:px-24 px-4 flex items-center justify-between'>
+      <div>
+        <LinksDropdown />
+      </div>
+      <div className='flex items-center gap-x-4'>
+        <ThemeToggle />
+        <UserButton afterSignOutUrl='/' />
+      </div>
+    </nav>
   )
 }
 export default NavBar
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,31 @@
+'use client';
+import Logo from '../assets/logo.svg';
+import links from '../utils/links';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Button } from './ui/button';
+import { usePathname } from 'next/navigation';
 const SideBar = () => {
+  const pathname = usePathname();
   return (
-    <div>SideBar</div>
-  )
-}
-export default SideBar
+    <aside className="py-4 px-8 bg-muted h-full">
+      <Image src={Logo} alt="logo" className="mx-auto" />
+      <div className="flex flex-col mt-20 gap-y-4">
+        {links.map(link => {
+          return (
+            <Button
+              asChild
+              key={link.href}
+              variant={pathname === link.href ? 'default' : 'link'}
+            >
+              <Link href={link.href} className="flex items-center gap-x-2 ">
+                {link.icon} <span className="capitalize">{link.label}</span>
+              </Link>
+            </Button>
+          );
+        })}
+      </div>
+    </aside>
+  );
+};
+export default SideBar;
