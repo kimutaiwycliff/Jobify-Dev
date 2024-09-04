8c36d75aa49503d45708227689234f4dbca4a93c
Wycliff Kimutai <kcliff@tutanota.com>
2024-09-04 11:59:03 +0300

    All complete

--- app/(dashboard)/jobs/page.tsx
+++ app/(dashboard)/jobs/page.tsx
@@ -1,6 +1,19 @@
-const AllJobsPage = () => {
+import JobsList from '@/components/JobsList';
+import SearchForm from '@/components/SearchForm';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { getAllJobsAction } from '@/utils/actions';
+const AllJobsPage = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['jobs', '', 'all', 1],
+    queryFn: () => getAllJobsAction({}),
+  });
   return (
-    <div>AllJobsPage</div>
-  )
-}
-export default AllJobsPage
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <SearchForm />
+      <JobsList />
+    </HydrationBoundary>
+  );
+};
+export default AllJobsPage;
--- app/providers.tsx
+++ app/providers.tsx
@@ -2,12 +2,31 @@
 
 import { ThemeProvider } from '@/components/Theme-Provider';
 import { PropsWithChildren } from 'react';
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from '@/components/ui/toaster';
 
 const Providers = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(() => {
+    return new QueryClient({
+      defaultOptions: {
+        queries: {
+          // With SSR, we usually want to set some default staleTime
+          // above 0 to avoid refetching immediately on the client
+          staleTime: 60 * 1000 * 5,
+        },
+      },
+    });
+  });
   return (
     <>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-        {children}
+        <Toaster />
+        <QueryClientProvider client={queryClient}>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   );
